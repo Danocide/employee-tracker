29282e8625d48b9f088227aa62ee8a6435dc2796
danocide <dan.c.reilly@gmail.com>
2023-02-02 21:53:35 -0500

    added prompt for main question to selext your action and added switch for each action

--- index.js
+++ index.js
@@ -3,8 +3,43 @@ const inquirer = require('inquirer')
 require('console.table')
 
 if (connection) {
-    console.log("Datebase is running")
+    console.log("Connection Made")
     mainQuestion()
 }
 
-
+function mainQuestion() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainQuestion',
+            message: "What would you like to do?",
+            choices: ['View All', 'View Employees', 'View Departments', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee', 'Quit']
+        }
+    ]).then(answer => {
+        switch (answer.mainQuestion) {
+            case 'View All':
+                viewAll()
+                break;
+            case 'View Employees':
+                viewEmployees()
+                break;
+            case 'View Departments':
+                viewDeparments()
+                break;
+            case 'Add Department':
+                addDepartment()
+                break;
+            case 'Add Role':
+                addRole()
+                break;
+            case 'Add Employee':
+                addEmployee()
+                break;
+            case 'Update Employee':
+                updateEmployeeRole()
+                break;
+            default:
+                connection.end()
+        }
+    })
+};
